783260a2c05959dd3f06bd33ce706b92b9c5cc3f
jupiterzako <97920580+jupiterzako@users.noreply.github.com>
2022-10-11 19:01:32 +0200

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+export var speed = 100
+var screen_size 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ playerSprite.gd
@@ -0,0 +1,8 @@
+extends Sprite
+
+func _process(delta:float) -> void:
+	look_at(get_global_mouse_position())
+
+	rotation_degrees = rotation_degrees + 90 
+
+
