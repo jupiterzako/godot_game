e2b7d1393d4465155090a528680a2ac2bbadfe95
jupiterzako <97920580+jupiterzako@users.noreply.github.com>
2022-10-11 16:13:15 +0200

    Add files via upload

--- GodotGame/playerSprite.gd
+++ GodotGame/playerSprite.gd
@@ -2,18 +2,7 @@ extends Sprite
 
 func _process(delta:float) -> void:
 	look_at(get_global_mouse_position())
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 	rotation_degrees = rotation_degrees + 90 
 
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
