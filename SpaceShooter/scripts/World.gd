8cd8b8b8c1c8ad6d78601d7123ac61ceca992a3b
juhum <68527324+juhum@users.noreply.github.com>
2022-12-14 18:36:19 +0100

    enemies now can spawn on the whole map and won't spawn in player's view

--- SpaceShooter/scripts/World.gd
+++ SpaceShooter/scripts/World.gd
@@ -8,7 +8,8 @@ var preEnemies = [
 
 
 onready var enemy_timer = $Enemy_Spawn_Timer
-var next_spawn = 1
+var next_spawn = 0.5
+
 
 
 
@@ -19,9 +20,11 @@ func _ready():
 func _on_Enemy_Spawn_Timer_timeout():
 	var enemies = preEnemies[randi()% preEnemies.size()]
 	var enemy = enemies.instance()
-	enemy.position = Vector2(rand_range(-1500,1500),rand_range(-1000,1000))
-	while enemy.position.x < 1000 and enemy.position.x > -1000 and enemy.position.y <500 and  enemy.position.y > -500:
-		enemy.position = Vector2(rand_range(-1500,1500),rand_range(-1000,1000))
+	var p = get_node("Player").global_position
+	enemy.position = Vector2(rand_range(p.x+1500,p.x-1500),rand_range(p.y+1500,p.y-1500))
+	while enemy.position.x < p.x+1000 and enemy.position.x > p.x-1000 and enemy.position.y < p.y+1000 and  enemy.position.y > p.y-1000:
+		p = get_node("Player").global_position
+		enemy.position = Vector2(rand_range(p.x+1500,p.x-1500),rand_range(p.y+1500,p.y-1500))
 	
 	get_tree().current_scene.add_child(enemy)
 	enemy_timer.start(next_spawn)
